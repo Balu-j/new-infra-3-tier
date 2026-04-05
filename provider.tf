4443242f0af5abe8415f170880897aad68f29a99
Balu-j <balajijanjanamm@gmail.com>
2026-04-06 01:40:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.0" # Usually better to use 5.x for now
     }
   }
 }
-
